feafbc9a3958ee87b94fd57cc24097ce3dc7c556
Himanshu Ranjan <himanshuranjan.cse@gmail.com>
2026-06-07 21:58:39 +0530

    Upgrade GAN to DCGAN with Conv layers for 64x64 X-ray images

--- model/gan.py
+++ model/gan.py
@@ -2,39 +2,69 @@ import torch
 import torch.nn as nn
 
 class Generator(nn.Module):
-    """Takes random noise, outputs a fake image."""
-    def __init__(self, noise_dim=100, img_channels=1, img_size=28):
+    """
+    Takes random noise, outputs a 64x64 fake chest X-ray.
+    Uses ConvTranspose2d layers to upsample noise into an image.
+    """
+    def __init__(self, noise_dim=100):
         super(Generator, self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(noise_dim, 256),
-            nn.ReLU(),
-            nn.Linear(256, 512),
-            nn.ReLU(),
-            nn.Linear(512, img_channels * img_size * img_size),
-            nn.Tanh()  # output between -1 and 1
+            # input: noise_dim x 1 x 1
+            nn.ConvTranspose2d(noise_dim, 512, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            # 512 x 4 x 4
+            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            # 256 x 8 x 8
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            # 128 x 16 x 16
+            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            # 64 x 32 x 32
+            nn.ConvTranspose2d(64, 1, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # output: 1 x 64 x 64
         )
-        self.img_channels = img_channels
-        self.img_size = img_size
 
     def forward(self, noise):
-        img = self.model(noise)
-        img = img.view(img.size(0), self.img_channels, self.img_size, self.img_size)
-        return img
+        noise = noise.view(noise.size(0), noise.size(1), 1, 1)
+        return self.model(noise)
 
 
 class Discriminator(nn.Module):
-    """Takes an image, outputs probability of being real."""
-    def __init__(self, img_channels=1, img_size=28):
+    """
+    Takes a 64x64 image, outputs probability of being a real X-ray.
+    Uses Conv2d layers to downsample image into a single score.
+    """
+    def __init__(self):
         super(Discriminator, self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(img_channels * img_size * img_size, 512),
-            nn.LeakyReLU(0.2),
-            nn.Linear(512, 256),
-            nn.LeakyReLU(0.2),
-            nn.Linear(256, 1),
-            nn.Sigmoid()  # output 0 (fake) or 1 (real)
+            # input: 1 x 64 x 64
+            nn.Conv2d(1, 64, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 64 x 32 x 32
+            nn.Conv2d(64, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 128 x 16 x 16
+            nn.Conv2d(128, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 256 x 8 x 8
+            nn.Conv2d(256, 512, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 512 x 4 x 4
+            nn.Conv2d(512, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+            # output: 1 x 1 x 1
         )
 
     def forward(self, img):
-        img_flat = img.view(img.size(0), -1)
-        return self.model(img_flat)
+        out = self.model(img)
+        return out.view(out.size(0), 1)  # flatten to [batch, 1]
